30c55baca0a4ef3abf04cda8e1bff29d6dee120e
john <pratheepjohn5@gmail.com>
2026-03-15 09:29:41 +0530

    deploy project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
@@ -13,6 +12,8 @@ root.render(
 
 )
 
-console.log(serviceWorkerRegistration)
+
 serviceWorkerRegistration.register()
+
+
 
