2e653c5eb612a0769d42b326ae59dda86a61470a
john <pratheepjohn5@gmail.com>
2026-03-29 17:45:02 +0530

    added razorpay verification

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const crypto = require("crypto");
 
 const {GoogleGenAI} = require('@google/genai')
 
@@ -8,10 +9,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+
 const genAI = new GoogleGenAI({
   
   apiKey: process.env.GEMINI_API_KEY,
-
+  
 
 
 })
@@ -43,6 +46,48 @@ ${userMessage}
     })
   }
 })
+
+
+
+
+app.post("/create-order", async (req, res) => {
+
+  console.log(req.body)
+
+  const options = {
+  
+    amount: req.body.amount * 100, // in paise
+    currency: "INR",
+    receipt: "receipt_order_1",
+  
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+
+app.post("/verify-payment", (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+  const generated_signature = crypto
+    .createHmac("sha256", "rzp_test_SS7LhR6CQXXuvA")
+    .update(razorpay_order_id + "|" + razorpay_payment_id)
+    .digest("hex");
+
+  if (generated_signature === razorpay_signature) {
+    res.send({ success: true });
+  } else {
+    res.send({ success: false });
+  }
+});
+
+
 
 app.listen(5000, () => console.log('Server running on 5000'))
 
